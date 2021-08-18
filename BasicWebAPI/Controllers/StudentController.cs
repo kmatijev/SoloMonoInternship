31c1480987d595917f61ed0f59c85c1b1e248c14
Karlo Matijević <karlo.mat19@gmail.com>
2021-08-18 18:12:51 +0200

    Student and SQL DB done

--- BasicWebAPI/Controllers/StudentController.cs
+++ BasicWebAPI/Controllers/StudentController.cs
@@ -1,81 +1,163 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace BasicWebAPI.Controllers
 {
+    public class Grade
+    {
+        public int id;
+        public string name;
 
+    }
     public class Student
     {
-        public static Dictionary<int, string> Data = new Dictionary<int, string>();
+        public int id;
+        public string name;
+        public int gradeId;
     }
 
     public class StudentController : ApiController
     {
+        public SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
 
         // GET api/student/{id}
         [HttpGet]
         [Route("api/student/{id}")]
         public HttpResponseMessage Get(int id)
         {
-            if (Student.Data.ContainsKey(id))
+            SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+
+            List<Student> StudentList = new List<Student>();
+
+            using (connection)
             {
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, Student.Data[id]);
-                return Msg;
-            }
-            else
-            {
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.NotFound, id);
-                return Msg;
+                connection.Open();
+                string queryString = String.Format("SELECT StudentName, GradeID FROM STUDENT WHERE StudentID = {0};", id);
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Student S = new Student();
+                        S.name = Convert.ToString(reader.GetString(0));
+                        S.gradeId = Convert.ToInt32(reader.GetInt32(1));
+                        S.id = id;
+
+                        StudentList.Add(S);
+                    }
+                    reader.NextResult();
+                    string combinedString = "";
+                    foreach (Student x in StudentList)
+                    {
+                        combinedString += String.Format("Student ID: {0}, Student Name: {1}, Grade ID: {2}", x.id, x.name, x.gradeId);
+                    }
+                    connection.Close();
+
+                    HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, combinedString);
+                    return Msg;
+                }
+                else
+                {
+                    connection.Close();
+                    HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.BadRequest, "No student for given id.");
+                    return Msg;
+                }
             }
         }
         // POST api/student/{id}
         [HttpPost]
-        [Route("api/Student/{id}")]
-        public HttpResponseMessage Post(int id, [FromBody] string value)
+        [Route("api/student/{id}/{gradeId}")]
+        public HttpResponseMessage Post(int id, int gradeId, [FromBody] string value)
         {
-            if (!Student.Data.ContainsKey(id))
+            using (connection)
             {
-                Student.Data.Add(id, value);
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, Student.Data[id]);
-                return Msg;
-            }
-            else
-            {
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.Conflict, id);
-                return Msg;
-            }
-        }
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
 
-        [HttpPut]
-        [Route("api/Student/{id}")]
-        // PUT api/Student/5
-        public HttpResponseMessage Put(int id, [FromBody] string value)
-        {
-            Student.Data[id] = value;
-            HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, Student.Data[id]);
+                string queryString = String.Format("INSERT INTO STUDENT(StudentID, StudentName, GradeID) VALUES({0}, '{1}', {2});", id ,value, gradeId);
 
+                adapter.InsertCommand = new SqlCommand(queryString, connection);
+                adapter.InsertCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            string returnString = "StudentID: " + id + ", StudentName:" + value + " ,StudentID: " + gradeId;
+
+            HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.Created, returnString);
             return Msg;
         }
-        // DELETE api/Student/5
+
+        // PUT api/student/5
+        [HttpPut]
+        [Route("api/student/{id}")]
+        public HttpResponseMessage Put(int id, [FromBody] string value)
+        {
+            using (connection)
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT StudentID, StudentName FROM STUDENT", connection);
+
+                adapter.UpdateCommand = new SqlCommand("UPDATE STUDENT SET StudentName = @StudentName " + "WHERE StudentID = @StudentID", connection);
+
+                adapter.UpdateCommand.Parameters.Add("@StudentName", SqlDbType.NVarChar, 15).Value = value;
+
+
+                SqlParameter parameter = adapter.UpdateCommand.Parameters.Add("@StudentID", SqlDbType.Int);
+                parameter.SourceColumn = "StudentID";
+                parameter.SourceVersion = DataRowVersion.Original;
+
+                DataTable categoryTable = new DataTable();
+                adapter.Fill(categoryTable);
+
+                DataRow categoryRow = categoryTable.Rows[0];
+                categoryRow["StudentName"] = value;
+
+                adapter.Update(categoryTable);
+
+                string combinedString = "";
+
+                foreach (DataRow row in categoryTable.Rows)
+                {
+                    {
+                        combinedString += String.Format("{0}: {1}, ", row[0], row[1]);
+                    }
+                }
+                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, combinedString);
+                return Msg;
+            }
+        }
+        // DELETE api/student/5
         [HttpDelete]
-        [Route("api/Student/{id}")]
+        [Route("api/student/{id}")]
         public HttpResponseMessage Delete(int id)
         {
-            if (Student.Data.ContainsKey(id))
+            using (connection)
             {
-                Student.Data.Remove(id);
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, id);
-                return Msg;
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                string queryString = String.Format("DELETE STUDENT WHERE StudentID = {0}", id);
+
+                adapter.DeleteCommand = connection.CreateCommand();
+                adapter.DeleteCommand.CommandText = queryString;
+                adapter.DeleteCommand.ExecuteNonQuery();
+                connection.Close();
             }
-            else
-            {
-                HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.NotFound, id);
-                return Msg;
-            }
+
+            string returnString = "Deleted: StudentID: " + id;
+
+            HttpResponseMessage Msg = Request.CreateResponse(HttpStatusCode.OK, returnString);
+            return Msg;
         }
     }
 }
---
+++ BasicWebAPI/Query.sql
@@ -0,0 +1,33 @@
+﻿DROP TABLE STUDENT;
+DROP TABLE GRADE;
+
+CREATE TABLE Grade(
+GradeID INTEGER PRIMARY KEY,
+GradeName VARCHAR(15)
+);
+
+CREATE TABLE Student(
+StudentID INTEGER PRIMARY KEY,
+StudentName VARCHAR(15),
+GradeID INTEGER FOREIGN KEY REFERENCES Grade(GradeID)
+);
+
+
+INSERT INTO GRADE(GradeID, GradeName)
+VALUES (1, 'Catterpillars');
+INSERT INTO GRADE(GradeID, GradeName)
+VALUES (2,'Butterflys');
+
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (1, 'Marko', 1);
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (2, 'Darko', 1);
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (3, 'Parko', 1);
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (4, 'Karko', 2);
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (5, 'Larko', 2);
+INSERT INTO STUDENT(StudentID, StudentName, GradeID)
+VALUES (6, 'Sarko', 2);
+
