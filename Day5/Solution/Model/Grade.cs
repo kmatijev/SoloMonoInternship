7fa9e8d22dd0f57054ceca8efeea2fd14aa17d84
Karlo Matijević <karlo.mat19@gmail.com>
2021-08-20 14:57:24 +0200

    Day 5 done

---
+++ Day5/Solution/Model/Grade.cs
@@ -0,0 +1,11 @@
+﻿using Model.Common.Grade;
+
+namespace Model.Grade
+{
+    public class Grade : IGrade
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ Day5/Solution/Service/Service.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Model.Grade;
+using Model.Student;
+using Service.Common;
+using WebRepository;
+
+namespace DService
+{
+    public class Service : IService
+    {
+        private Repository Repo = new Repository();
+        public async Task<List<Grade>> GetGrade(int id) => await Repo.GetGrade(id);
+        public async Task<Grade> PostGrade(int id, string value) => await Repo .PostGrade(id, value);
+        public async Task<string> PutGrade(int id, string value) => await Repo.PutGrade(id, value);
+        public async Task<string> DeleteGrade(int id) => await Repo .DeleteGrade(id);
+
+        public async Task<List<Student>> GetStudent(int id) => await Repo .GetStudent(id);
+        public async Task<Student> PostStudent(int id, int gradeId, string value) => await Repo.PostStudent(id, gradeId, value);
+        public async Task<string> PutStudent(int id, string value) => await Repo.PutStudent(id, value);
+        public async Task<string> DeleteStudent(int id) => await Repo.DeleteStudent(id);
+    }
+}
